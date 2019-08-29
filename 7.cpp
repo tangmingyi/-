db8dc40e5fdfd265f65d062c7c969c5cbe57dfe6
tmy_notebook <zb19851222@qq.com>
2019-08-29 14:42:02 +0800

    7 推荐 动态规划

--- 7.cpp
+++ 7.cpp
@@ -9,36 +9,41 @@ void initVector(std::vector<int >& value){
         std::cin>>*Iter;
     }
 }
-void initDp(std::vector<std::vector<int >>& dp, const std::vector<int >& value){
+void initDp(std::vector<std::vector<int >>& dp, const std::vector<int >& value,int n){
     auto Iter = dp.begin() +2;
     auto IterValue = value.begin() +2;
     for(Iter;Iter!=dp.end();Iter++){
         (*Iter)[0] = (*(Iter-1))[0] + std::abs(*IterValue - *(IterValue-1));
         IterValue++;
     }
+    dp[2][1] = std::abs(value[1]-value[0]);
+    for(int i=3;i<n;i++){
+        dp[i][i-1] = dp[i-1][i-2] + std::abs(value[i-1]-value[i-2]);
+    }
 }
 void fillDp(std::vector<std::vector<int >>& dp, const std::vector<int>& value,int n){
-    for(int i=1;i<n;i++){
-//        dp[i][i-1] = ;
-        for(int j=1;j<i;j++){
-           dp[i][j] = dp[i][j-1] + std::abs(value[j]-value[j-1]);
-
+    for(int i=2;i<n;i++){
+//        dp[i][i-1] = dp[i-1][i-2] + std::abs(value[i]-value[i-2]);
+        for(int j=0;j<i-1;j++){
+           dp[i][j] = dp[i-1][j] + std::abs(value[i]-value[i-1]);
+           dp[i][i-1] = std::min(dp[i][i-1],dp[i-1][j] + std::abs(value[j]-value[i]));
         }
     }
 }
 int main(){
     int n;
     std::cin>>n;
+//    n = 8;
     std::vector<int > value(n,0);
     std::vector<std::vector<int> > dp(n,std::vector<int >(n,0));
     initVector(value);
-    initDp(dp,value);
+    initDp(dp,value,n);
     fillDp(dp,value,n);
-    auto Iter = (*dp.end()).begin();
-    int Min = *Iter;
-    for(Iter;Iter!=(*dp.end()).end();Iter++){
-        if(*Iter<Min){
-            Min = *Iter;
+    auto listIter = dp[n-1].begin();
+    int Min = *listIter;
+    for(listIter;listIter!=dp[n-1].end()-1;listIter++){
+        if(*listIter<Min){
+            Min = *listIter;
         }
     }
     std::cout<<Min;
